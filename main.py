3dd7890f1add05ec2c6daf4915f05252204b4043
HaiLam <125887432+IceMonster2211@users.noreply.github.com>
2025-08-11 15:36:55 +0700

    Week4

--- main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from src.api.main_router import router as main_router
-
-app = FastAPI()
-app.include_router(main_router)
--- src/api/main_router.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from src.api.hello_world.main import router as hello_world_router
-
-router = APIRouter()
-
-router.include_router(hello_world_router)
