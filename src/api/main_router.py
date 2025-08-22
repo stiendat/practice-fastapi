7c16d3562c4ff38aa31bf033ff1478570721a884
Andy583 <honganhpham5803@gmail.com>
2025-08-22 15:07:05 +0700

    homework_w4 - PHA

--- src/api/main_router.py
+++ src/api/main_router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from src.api.hello_world.main import router as hello_world_router
+from src.api.to_do_service import router as todo_router
 
 router = APIRouter()
 
 router.include_router(hello_world_router)
+router.include_router(todo_router)
